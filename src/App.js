40570290a4a02b3bf1d4fd71652633253a3cecfe
ProDesignerNurul <maxnewsbd@gmail.com>
2022-10-07 12:07:40 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
+import Main from './Main/Main';
 import Shop from './components/Shop/Shop';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/', element: <Main></Main>, children: [
+        {
+          path: '/', element: <Shop></Shop>
+        },
+        {
+          path: '/shop', element: <Shop></Shop>
+        }
+      ]
+    }
+  ])
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
